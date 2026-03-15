6d75851b72885ee38ade71e708817c39e8d658b9
nstechbytes <nstechbytes@gmail.com>
2026-03-15 21:00:44 +0500

    feat(hotkey): add WASD key hotkey support with UI feedback.

--- Widgets/src/index.js
+++ Widgets/src/index.js
@@ -1,13 +1,54 @@
-import { widgetWindow, addon } from 'novadesk'
+import { widgetWindow, addon } from "novadesk";
 
 const widget = new widgetWindow({
-    id: "Hotkey",
-    script: "ui/script.ui.js"
-})
+  id: "Hotkey",
+  script: "ui/script.ui.js",
+});
 
-const hotkeyAddon = addon.load("D:\\Novadesk-Project\\Hotkey\\dist\\x64\\Debug\\Hotkey\\Hotkey.dll")
+const hotkeyAddon = addon.load(
+  "D:\\Novadesk-Project\\Hotkey\\dist\\x64\\Debug\\Hotkey\\Hotkey.dll"
+);
 
-const w_ID = hotkeyAddon.register("W", {
-    onKeyDown: function () { console.log("CTRL+SHIFT+M down"); },
-    onKeyUp: function () { console.log("CTRL+SHIFT+M up"); }
+const w_hotkey = hotkeyAddon.register("W", {
+  onKeyDown: function () {
+    ipcMain.send("w_keyDown", { key: "W" });
+    // console.log("W down");
+  },
+  onKeyUp: function () {
+    ipcMain.send("w_keyUp", { key: "W" });
+    // console.log("W up");
+  },
+});
+
+const a_hotkey = hotkeyAddon.register("A", {
+  onKeyDown: function () {
+    ipcMain.send("a_keyDown", { key: "A" });
+    // console.log("A down");
+  },
+  onKeyUp: function () {
+    ipcMain.send("a_keyUp", { key: "A" });
+    // console.log("A up");
+  },
+});
+
+const s_hotkey = hotkeyAddon.register("S", {
+  onKeyDown: function () {
+    ipcMain.send("s_keyDown", { key: "S" });
+    // console.log("S down");
+  },
+  onKeyUp: function () {
+    ipcMain.send("s_keyUp", { key: "S" });
+    // console.log("S up");
+  },
+});
+
+const d_hotkey = hotkeyAddon.register("D", {
+  onKeyDown: function () {
+    ipcMain.send("d_keyDown", { key: "D" });
+    // console.log("D down");
+  },
+  onKeyUp: function () {
+    ipcMain.send("d_keyUp", { key: "D" });
+    // console.log("D up");
+  },
 });
--- Widgets/src/ui/script.ui.js
+++ Widgets/src/ui/script.ui.js
@@ -3,28 +3,34 @@
 //  =================================================================
 
 // appearance
-let button_backgroundColor = "rgba(30,30,30,0.8)"
-let button_strokeColor = "rgba(217, 217, 217, 0.8)"
-let button_Radius = 10
-let strokeWidth = 2
-let fontColor = "rgb(255, 255, 255)"
-let fontSize = 30
+let button_backgroundColor = "rgba(30,30,30,0.8)";
+let button_strokeColor = "rgba(217, 217, 217, 0.8)";
+let button_strokeColor_Accent = "rgb(4, 255, 100)";
+let button_Radius = 10;
+let strokeWidth = 2;
+let fontColor = "rgb(255, 255, 255)";
+let fontColor_Accent = "rgb(4, 255, 100)";
+let fontSize = 30;
 
 // dimensions
-let button_width = 70
-let button_height = 70
+let button_width = 70;
+let button_height = 70;
 
-let padY = 5
-let padX = 5
+let padY = 5;
+let padX = 5;
 
 //  =================================================================
 //  Helper Functions
 //  =================================================================
 
 function getElementProperty(id, property) {
-    result = ui.getElementProperty(id, property)
-    // console.log("Result: " + property + " = " + result)
-    return result
+  result = ui.getElementProperty(id, property);
+  // console.log("Result: " + property + " = " + result)
+  return result;
+}
+
+function setElementProperty(id, property, value) {
+  ui.setElementProperties(id, { [property]: value });
 }
 
 //  =================================================================
@@ -32,106 +38,166 @@ function getElementProperty(id, property) {
 //  =================================================================
 
 ui.addShape({
-    type: "rectangle",
-    id: "background",
-    width: 220,
-    height: 150,
-    fillColor: "rgba(255, 255, 255, 0)",
-    strokeWidth: 0,
-})
+  type: "rectangle",
+  id: "background",
+  width: 230,
+  height: 150,
+  fillColor: "rgba(255, 255, 255, 0)",
+  strokeWidth: 0,
+});
 
 ui.addShape({
-    type: "rectangle",
-    id: "w_Button",
-    x: (button_width + strokeWidth + padX),
-    y: (strokeWidth),
-    width: button_width,
-    height: button_height,
-    radius: button_Radius,
-    fillColor: button_backgroundColor,
-    strokeColor: button_strokeColor,
-    strokeWidth: 2,
-})
+  type: "rectangle",
+  id: "w_Button",
+  x: button_width + strokeWidth + padX,
+  y: strokeWidth,
+  width: button_width,
+  height: button_height,
+  radius: button_Radius,
+  fillColor: button_backgroundColor,
+  strokeColor: button_strokeColor,
+  strokeWidth: 2,
+});
 
 ui.addText({
-    type: "text",
-    id: "w_Button_Text",
-    text: "W",
-    x: getElementProperty("w_Button", "x") + (getElementProperty("w_Button", "width") / 2),
-    y: getElementProperty("w_Button", "y") + (getElementProperty("w_Button", "height") / 2),
-    fontColor: fontColor,
-    fontSize: fontSize,
-    textAlign: "centercenter",
-})
+  type: "text",
+  id: "w_Button_Text",
+  text: "W",
+  x:
+    getElementProperty("w_Button", "x") +
+    getElementProperty("w_Button", "width") / 2,
+  y:
+    getElementProperty("w_Button", "y") +
+    getElementProperty("w_Button", "height") / 2,
+  fontColor: fontColor,
+  fontSize: fontSize,
+  textAlign: "centercenter",
+});
 
 ui.addShape({
-    type: "rectangle",
-    id: "a_Button",
-    x: (strokeWidth),
-    y: (button_height + strokeWidth + padY),
-    width: button_width,
-    height: button_height,
-    radius: button_Radius,
-    fillColor: button_backgroundColor,
-    strokeColor: button_strokeColor,
-    strokeWidth: 2,
-})
+  type: "rectangle",
+  id: "a_Button",
+  x: strokeWidth,
+  y: button_height + strokeWidth + padY,
+  width: button_width,
+  height: button_height,
+  radius: button_Radius,
+  fillColor: button_backgroundColor,
+  strokeColor: button_strokeColor,
+  strokeWidth: 2,
+});
 
 ui.addText({
-    type: "text",
-    id: "a_Button_Text",
-    text: "A",
-    x: getElementProperty("a_Button", "x") + (getElementProperty("a_Button", "width") / 2),
-    y: getElementProperty("a_Button", "y") + (getElementProperty("a_Button", "height") / 2),
-    fontColor: fontColor,
-    fontSize: fontSize,
-    textAlign: "centercenter",
-})
+  type: "text",
+  id: "a_Button_Text",
+  text: "A",
+  x:
+    getElementProperty("a_Button", "x") +
+    getElementProperty("a_Button", "width") / 2,
+  y:
+    getElementProperty("a_Button", "y") +
+    getElementProperty("a_Button", "height") / 2,
+  fontColor: fontColor,
+  fontSize: fontSize,
+  textAlign: "centercenter",
+});
 
 ui.addShape({
-    type: "rectangle",
-    id: "s_Button",
-    x: (button_width + strokeWidth + padX),
-    y: (button_height + strokeWidth + padY),
-    width: button_width,
-    height: button_height,
-    radius: button_Radius,
-    fillColor: button_backgroundColor,
-    strokeColor: button_strokeColor,
-    strokeWidth: 2,
-})
+  type: "rectangle",
+  id: "s_Button",
+  x: button_width + strokeWidth + padX,
+  y: button_height + strokeWidth + padY,
+  width: button_width,
+  height: button_height,
+  radius: button_Radius,
+  fillColor: button_backgroundColor,
+  strokeColor: button_strokeColor,
+  strokeWidth: 2,
+});
 
 ui.addText({
-    type: "text",
-    id: "s_Button_Text",
-    text: "S",
-    x: getElementProperty("s_Button", "x") + (getElementProperty("s_Button", "width") / 2),
-    y: getElementProperty("s_Button", "y") + (getElementProperty("s_Button", "height") / 2),
-    fontColor: fontColor,
-    fontSize: fontSize,
-    textAlign: "centercenter",
-})
+  type: "text",
+  id: "s_Button_Text",
+  text: "S",
+  x:
+    getElementProperty("s_Button", "x") +
+    getElementProperty("s_Button", "width") / 2,
+  y:
+    getElementProperty("s_Button", "y") +
+    getElementProperty("s_Button", "height") / 2,
+  fontColor: fontColor,
+  fontSize: fontSize,
+  textAlign: "centercenter",
+});
 
 ui.addShape({
-    type: "rectangle",
-    id: "d_Button",
-    x: ((button_width + strokeWidth + padX) * 2),
-    y: (button_height + strokeWidth + padY),
-    width: button_width,
-    height: button_height,
-    radius: button_Radius,
-    fillColor: button_backgroundColor,
-    strokeColor: button_strokeColor,
-    strokeWidth: 2,
-})
+  type: "rectangle",
+  id: "d_Button",
+  x: (button_width + strokeWidth + padX) * 2,
+  y: button_height + strokeWidth + padY,
+  width: button_width,
+  height: button_height,
+  radius: button_Radius,
+  fillColor: button_backgroundColor,
+  strokeColor: button_strokeColor,
+  strokeWidth: 2,
+});
 
 ui.addText({
-    type: "text",
-    id: "d_Button_Text",
-    text: "D",
-    x: getElementProperty("d_Button", "x") + (getElementProperty("d_Button", "width") / 2),
-    y: getElementProperty("d_Button", "y") + (getElementProperty("d_Button", "height") / 2),
-    fontColor: fontColor,
-    fontSize: fontSize,
-    textAlign: "centercenter",
-})
+  type: "text",
+  id: "d_Button_Text",
+  text: "D",
+  x:
+    getElementProperty("d_Button", "x") +
+    getElementProperty("d_Button", "width") / 2,
+  y:
+    getElementProperty("d_Button", "y") +
+    getElementProperty("d_Button", "height") / 2,
+  fontColor: fontColor,
+  fontSize: fontSize,
+  textAlign: "centercenter",
+});
+
+//  =================================================================
+//  IPC
+//  =================================================================
+
+ipcRenderer.on("w_keyDown", (event, payload) => {
+  setElementProperty("w_Button", "strokeColor", button_strokeColor_Accent);
+  setElementProperty("w_Button_Text", "fontColor", fontColor_Accent);
+});
+
+ipcRenderer.on("w_keyUp", (event, payload) => {
+  setElementProperty("w_Button", "strokeColor", button_strokeColor);
+  setElementProperty("w_Button_Text", "fontColor", fontColor);
+});
+
+ipcRenderer.on("a_keyDown", (event, payload) => {
+  setElementProperty("a_Button", "strokeColor", button_strokeColor_Accent);
+  setElementProperty("a_Button_Text", "fontColor", fontColor_Accent);
+});
+
+ipcRenderer.on("a_keyUp", (event, payload) => {
+  setElementProperty("a_Button", "strokeColor", button_strokeColor);
+  setElementProperty("a_Button_Text", "fontColor", fontColor);
+});
+
+ipcRenderer.on("s_keyDown", (event, payload) => {
+  setElementProperty("s_Button", "strokeColor", button_strokeColor_Accent);
+  setElementProperty("s_Button_Text", "fontColor", fontColor_Accent);
+});
+
+ipcRenderer.on("s_keyUp", (event, payload) => {
+  setElementProperty("s_Button", "strokeColor", button_strokeColor);
+  setElementProperty("s_Button_Text", "fontColor", fontColor);
+});
+
+ipcRenderer.on("d_keyDown", (event, payload) => {
+  setElementProperty("d_Button", "strokeColor", button_strokeColor_Accent);
+  setElementProperty("d_Button_Text", "fontColor", fontColor_Accent);
+});
+
+ipcRenderer.on("d_keyUp", (event, payload) => {
+  setElementProperty("d_Button", "strokeColor", button_strokeColor);
+  setElementProperty("d_Button_Text", "fontColor", fontColor);
+});
